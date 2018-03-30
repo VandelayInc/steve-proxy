2cb5020b999fab520fdcd1ea9cfb3d3346136a8d
Steve Hallam <StevenAHallam@gmail.com>
2018-03-29 17:34:31 -0700

    Modified proxy to use layout of orignial proxy with SSR

--- templates/app.js
+++ templates/app.js
@@ -1,4 +1,25 @@
 
 module.exports = (neighborhood, reviews, description, bookings) => `
-  <div id="Neighborhood">${neighborhood}</div>
+  <div class='full-listing-page'>
+    <div class='top-bar'><img src='../../8.png'><img src='../../9.png'></div>
+    <div class='pictures' id='photo-carousel-service'><img src='../../listing_photo.jpg'></div>
+    <div class='center-column'>
+
+      <div class='details'>
+        <div id='description-service'>
+          <img src='../../1.png'>
+          <img src='../../2.png'>
+        </div>
+        <div id='reviews-service'><img src='../../3.png'></div>
+        <div class='host'><img src='../../4.png'></div>
+        <div id='Neighborhood'><img src='../../5.png'>${neighborhood}</div>
+      </div>
+
+      <div class='booking' id='bookings-service'><img src='../../booking.png'></div>
+
+    </div>
+
+    <div id='listings'><img src='../../6.png'></div>
+    <div class='bottom-bar'><img src='../../7.png'></div>
+  </div>
 `;
